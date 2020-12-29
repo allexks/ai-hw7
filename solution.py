3b5fab8af486c13d8f1b6b5d94ba0e7f21d7e0fb
Alexander Ignatov <yalishanda@abv.bg>
2020-12-30 00:37:19 +0200

    Parse and store points

--- solution.py
+++ solution.py
@@ -2,6 +2,23 @@
 
 """62136."""
 
+from dataclasses import dataclass
+from typing import Set
+
+
+@dataclass(frozen=True)
+class Point:
+    x: float
+    y: float
+
+
+class KMeans:
+    def __init__(self, clusters: int, points: Set[Point]):
+        self.clusters = clusters
+        self.points_to_clusters_dict = {p: 0 for p in points}
+        self.clusters_to_points_dict = {0: points}
+
+    # TODO
 
 if __name__ == "__main__":
     from argparse import ArgumentParser, FileType
@@ -24,6 +41,20 @@ if __name__ == "__main__":
         action="store",
         dest="clusters"
     )
+
     args = parser.parse_args()
-    print(args.dataset_file)
-    print(args.clusters)
+
+    points = {
+        Point(p[0], p[1])
+        for p in (
+            [*map(float, line.split())]
+            for line in args.dataset_file.readlines()
+            if len(line.split()) == 2
+        )
+    }
+
+    args.dataset_file.close()
+
+    kMeans = KMeans(args.clusters, points)
+
+    # TODO
