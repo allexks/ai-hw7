12535d276db55a6d502582f5ee093998dbbe6e6a
Alexander Ignatov <yalishanda@abv.bg>
2020-12-30 01:15:13 +0200

    Add plotting

--- solution.py
+++ solution.py
@@ -3,8 +3,7 @@
 """62136."""
 
 from dataclasses import dataclass
-from typing import Set
-
+from typing import List
 
 @dataclass(frozen=True)
 class Point:
@@ -13,12 +12,13 @@ class Point:
 
 
 class KMeans:
-    def __init__(self, clusters: int, points: Set[Point]):
+    def __init__(self, clusters: int, points: List[Point]):
         self.clusters = clusters
         self.points_to_clusters_dict = {p: 0 for p in points}
-        self.clusters_to_points_dict = {0: points}
+        self.clusters_to_points_dict = {0: set(points)}
 
-    # TODO
+    def fit(self):
+        pass #TODO
 
 if __name__ == "__main__":
     from argparse import ArgumentParser, FileType
@@ -44,17 +44,23 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    points = {
+    points = [
         Point(p[0], p[1])
         for p in (
             [*map(float, line.split())]
             for line in args.dataset_file.readlines()
             if len(line.split()) == 2
         )
-    }
+    ]
 
     args.dataset_file.close()
 
     kMeans = KMeans(args.clusters, points)
+    kMeans.fit()
 
-    # TODO
+    import matplotlib.pyplot as plt
+    x = [p.x for p in points]
+    y = [p.y for p in points]
+    c = [kMeans.points_to_clusters_dict[p] for p in points]
+    plt.scatter(x=x, y=y, c=c)
+    plt.show()
