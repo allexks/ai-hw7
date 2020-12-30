fefcff0353f3144efaa04114cf3d0b58738d8f80
Alexander Ignatov <yalishanda@abv.bg>
2020-12-30 15:39:55 +0200

    Implement cluster evaluation and random reinitialization of zero-size cluster centroids

--- solution.py
+++ solution.py
@@ -3,9 +3,11 @@
 """62136."""
 
 from dataclasses import dataclass
-from typing import List
+from typing import List, Tuple
 from random import random
 from math import sqrt, inf
+import matplotlib.pyplot as plt
+
 
 @dataclass(frozen=True)
 class Point:
@@ -34,27 +36,35 @@ class KMeans:
         self._max_y = max(map(lambda p: p.y, points))
 
     def fit(self):
-        while True: # loops until no 0-size cluster exist
-            means = self._random_means_init()
-            self._assignment_step(means)
+        means = self._random_means_init()
 
-            if not any(map(
-                lambda i: len(self.clusters_to_points_dict[i]) == 0,
-                range(self.clusters_count)
-            )):
-                break
-
-        iteration = 0
         is_over = False
+        iteration = 0
         while not is_over:
-            if iteration > 0:
-                self._assignment_step(means)
+            self._assignment_step(means)
 
             is_over = self._update_step(means)
 
             debug = "-".join(str(len(self.clusters_to_points_dict[i])) for i in range(self.clusters_count))
             print(f"({iteration}) {debug}")
             iteration += 1
+
+    def evaluate_clusters(self) -> Tuple[float, float]:
+        """
+        Use single-linkage intercluster distance evaluation.
+        Return (minimum_distance, average_distance).
+        """
+        cl_eval = []
+        for cl1_id in range(self.clusters_count):
+            for cl2_id in filter(lambda i: i != cl1_id, range(self.clusters_count)):
+                cl_eval.append(min(
+                    p1.distance_to(p2)
+                    for p1 in self.clusters_to_points_dict[cl1_id]
+                    for p2 in self.clusters_to_points_dict[cl2_id]
+                    if p1 != p2
+                ))
+
+        return min(cl_eval), (sum(cl_eval) / len(cl_eval))
 
     def _random_means_init(self):
         return [
@@ -81,14 +91,30 @@ class KMeans:
         is_over = True
         for mean in means:
             cluster_points = self.clusters_to_points_dict[mean.id]
-            new_x = sum(p.x for p in cluster_points) / len(cluster_points)
-            new_y = sum(p.y for p in cluster_points) / len(cluster_points)
+
+            if len(cluster_points) == 0:
+                # Give it a new random location.
+                new_x = random() * self._max_x
+                new_y = random() * self._max_y
+            else:
+                new_x = sum(p.x for p in cluster_points) / len(cluster_points)
+                new_y = sum(p.y for p in cluster_points) / len(cluster_points)
+
             if new_x == mean.pos.x and new_y == mean.pos.y:
                 continue  # No update to this mean
             else:
                 is_over = False  # Update this mean which means another iteration should come.
                 means[mean.id] = KMeans.Mean(mean.id, Point(new_x, new_y))
+
         return is_over
+
+
+def plot_solution(solution: KMeans):
+        x = [p.x for p in points]
+        y = [p.y for p in points]
+        c = [solution.points_to_clusters_dict[p] for p in points]
+        plt.scatter(x=x, y=y, c=c)
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -126,12 +152,29 @@ if __name__ == "__main__":
 
     args.dataset_file.close()
 
-    kMeans = KMeans(args.clusters, points)
-    kMeans.fit()
+    solution = None
+    solution_eval = None
 
-    import matplotlib.pyplot as plt
-    x = [p.x for p in points]
-    y = [p.y for p in points]
-    c = [kMeans.points_to_clusters_dict[p] for p in points]
-    plt.scatter(x=x, y=y, c=c)
-    plt.show()
+    for _ in range(3):
+        kMeans = KMeans(args.clusters, points)
+        kMeans.fit()
+        print("Evaluating clusters...")
+        current = kMeans.evaluate_clusters()
+        print("Current solution: min={0}, avg={1}".format(*current))
+        if solution is None:
+            solution = kMeans
+            solution_eval = current
+        elif solution_eval[1] < current[1]:
+            print("Current is better.")
+            solution = kMeans
+            solution_eval = current
+        else:
+            print("Current is not better.")
+
+        plot_solution(kMeans)
+
+    assert solution is not None, "No solution."
+
+    plot_solution(solution)
+
+
